180930962ee83931e4e4b6fe610e5379db0dc6d7
Chip Bell <chip.bell@spinen.com>
2015-05-18 00:20:17 -0400

    Wiring it up. Crossing fingers

--- ellipse_draw.js
+++ ellipse_draw.js
@@ -6,7 +6,12 @@ var EllipseDraw = (function() {
         },
 
         redrawEllipse: function(points) {
-        
+            // calculate points on the ellipse
+            var edgePoints = Ellipse.edgePointsOfEllipse(points, Radius.currentValue());
+
+            // now draw them
+            var drawBlue = function(point) { Draw.drawPoint(point, 'blue'); };
+            edgePoints.forEach(drawBlue);
         },
     }
 })();
