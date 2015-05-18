f21d3456d79cf7eed1f86a573015c60eb43c2102
Chip Bell <chip.bell@spinen.com>
2015-05-18 07:54:58 -0400

    Redrawing the ellipse on the radius input's change

--- ellipse_draw.js
+++ ellipse_draw.js
@@ -1,13 +1,21 @@
 var EllipseDraw = (function() {
 
+    var currentPoints = [];
+
     return {
         init: function() {
-            Draw.onpointschanged = this.redrawEllipse;
+            Draw.onpointschanged = this.pointsChanged.bind(this);
+            document.getElementById('radius').oninput = this.redrawEllipse.bind(this);
         },
 
-        redrawEllipse: function(points) {
+        pointsChanged: function(points) {
+            currentPoints = points;
+            this.redrawEllipse();
+        },
+
+        redrawEllipse: function() {
             // calculate points on the ellipse
-            var edgePoints = Ellipse.edgePointsOfEllipse(points, Radius.currentValue());
+            var edgePoints = Ellipse.edgePointsOfEllipse(currentPoints, Radius.currentValue());
 
             // now draw them
             var drawBlue = function(point) { Draw.drawPoint(point, 'blue'); };
