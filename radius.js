bdf1b1ff623d8d8836e76c05d44e8bf444673e86
Chip Bell <chip.bell@spinen.com>
2015-05-17 23:41:39 -0400

    Adding a smaller radius listener

---
+++ radius.js
@@ -0,0 +1,28 @@
+var Radius = (function() {
+    var slider, label;
+
+    return {
+        /**
+         * Setup code for the radius slider
+         */
+        init: function() {
+            slider = document.getElementById('radius');
+            label = document.getElementById('radius-value');
+            slider.oninput = this.updateLabel;
+        },
+
+        /**
+         * Gets the current value of the radius slider
+         */
+        currentValue: function() {
+            return Number(slider.value);
+        },
+
+        /**
+         * Callback for when the label is updated
+         */
+        updateLabel: function() {
+            label.innerHTML = slider.value;
+        } 
+    };
+})();
