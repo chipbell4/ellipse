09556b00b4ff62cf350b658f1f258a0e5c7d5d3a
Chip Bell <chip.bell@spinen.com>
2015-05-25 20:03:15 -0400

    Trying out the loader

--- preset_loader.js
+++ preset_loader.js
@@ -8,13 +8,13 @@ var PresetLoader = (function() {
     presets.line = Preset.buildPreset(x, y, 50);
 
     // "sine" preset
-    var x = function(t) { return 400 + 100 * t; };
-    var y = function(t) { return 300 + 50 * Math.sin(t / 100); };
-    presets.sine = Preset.buildPreset(x, y, 50);
+    x = function(t) { return 400 + 200 * t; };
+    y = function(t) { return 300 + 50 * Math.sin(t / (2 * Math.PI) * 40); };
+    presets.sine = Preset.buildPreset(x, y, 200);
 
     return {
         init: function() {
-            Preset.loadPreset(presets.line);
+            Preset.loadPreset(presets.sine);
         }
     };
 })();
