f3c07ac21f33efb58112eb9f6ab4dfb97d95242f
Chip Bell <chip.bell@spinen.com>
2015-05-25 20:08:47 -0400

    Trying out a circle preset

--- preset_loader.js
+++ preset_loader.js
@@ -12,9 +12,14 @@ var PresetLoader = (function() {
     y = function(t) { return 300 + 50 * Math.sin(t / (2 * Math.PI) * 40); };
     presets.sine = Preset.buildPreset(x, y, 200);
 
+    // "circle" preset
+    x = function(t) { return 300 + 200 * Math.sin(t * 2 * Math.PI); };
+    y = function(t) { return 300 + 200 * Math.cos(t * 2 * Math.PI); };
+    presets.circle = Preset.buildPreset(x, y, 300);
+
     return {
         init: function() {
-            Preset.loadPreset(presets.sine);
+            Preset.loadPreset(presets.circle);
         }
     };
 })();
